188bc73c0bab25f4feaa3cd1c7fe8aed0f9b00ac
javier-pardinas <javierpb275@gmail.com>
2021-06-25 09:54:36 +0200

    declaring variables with types string, number and boolean

---
+++ first_app/types.ts
@@ -0,0 +1,9 @@
+//declare string (text):
+let vehicle: string;
+vehicle = "car";
+//declare number:
+let quantity: number;
+quantity = 1;
+//declare boolean (true, false):
+let isCar: boolean = true;
+
