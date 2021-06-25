2721d3ffe968a35bb4e58c87004111557a131b03
javier-pardinas <javierpb275@gmail.com>
2021-06-25 09:39:08 +0200

    created index.ts and function sumTs with typescript code. compiled code with tsc index.ts and then executed the file created index.js with node index.js.

---
+++ first_app/index.js
@@ -0,0 +1,4 @@
+var sumTs = function (value1, value2) {
+    return value1 + value2;
+};
+console.log(sumTs(1, 2));
---
+++ first_app/index.ts
@@ -0,0 +1,5 @@
+const sumTs = (value1: number, value2: number) => {
+    return value1 + value2;
+}
+
+console.log(sumTs(1, 2));
