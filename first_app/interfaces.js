dd19143340578a27a037245c09f983ee236de963
javier-pardinas <javierpb275@gmail.com>
2021-06-25 11:56:51 +0200

    created second interface billable and define properties in doctor and created function that accepts an object that is billable

--- first_app/interfaces.js
+++ first_app/interfaces.js
@@ -4,11 +4,18 @@ var Role;
     Role[Role["Cardiologist"] = 1] = "Cardiologist";
     Role[Role["Surgeon"] = 2] = "Surgeon";
 })(Role || (Role = {}));
+;
+;
+;
 var doctor = {
     name: 'Pepe',
     email: 'pepe@gmail.com',
-    role: Role.Surgeon
+    role: Role.Surgeon,
+    total: 25,
+    currentBill: function () { return "Value of bill: '" + this.total + "'"; }
 };
 //const printStaff = (staff: {name: string, email: string, role: Role}) => console.log(staff);
 var printStaff = function (staff) { return console.log(staff); };
+var printCurrentBill = function (bill) { return console.log(bill.currentBill()); };
 printStaff(doctor);
+printCurrentBill(doctor);
--- first_app/interfaces.ts
+++ first_app/interfaces.ts
@@ -2,21 +2,38 @@ enum Role {
     Neurologist,
     Cardiologist,
     Surgeon
-}
+};
 
-interface Staff {
+type StaffT = {
     name: string,
     email: string,
     role: Role
-}
+};
+
+interface StaffI {
+    name: string,
+    email: string,
+    role: Role
+};
+
+interface Billable {
+    total: number,
+    currentBill(): string
+};
 
 const doctor = {
     name: 'Pepe',
     email: 'pepe@gmail.com',
-    role: Role.Surgeon
-}
+    role: Role.Surgeon,
+    total: 25,
+    currentBill() { return `Value of bill: '${this.total}'`;}
+};
 
 //const printStaff = (staff: {name: string, email: string, role: Role}) => console.log(staff);
-const printStaff = (staff: Staff) => console.log(staff);
+const printStaff = (staff: StaffI) => console.log(staff);
+
+const printCurrentBill = (bill: Billable) => console.log(bill.currentBill());
 
 printStaff(doctor);
+
+printCurrentBill(doctor);
