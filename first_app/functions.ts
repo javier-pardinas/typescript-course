520a59a728ba11be8343c130e847bc16564f3c4c
javier-pardinas <javierpb275@gmail.com>
2021-06-25 11:07:03 +0200

    functions: the word never never: we never want to return anything.

--- first_app/functions.ts
+++ first_app/functions.ts
@@ -1,14 +1,14 @@
-const sum = (value1: number, value2: number):number => {
-    return value1 + value2;
-}
+const sum = (value1: number, value2: number):number =>  value1 + value2;
 
-const printSthV = (): void => {
-    console.log(sum(2, 2));
-}
+const printSthV = (): void => console.log(sum(2, 2));
 
 const printSthU = (): undefined => {
     console.log(sum(2, 2));
     return undefined;
+}
+
+const throwError = (message: string):never => {
+        throw new Error(message);
 }
 
 printSthV();
