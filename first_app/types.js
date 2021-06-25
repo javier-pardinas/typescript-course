79b89d93b875e00ecbafd5cf059853e283dc3c16
javier-pardinas <javierpb275@gmail.com>
2021-06-25 10:05:26 +0200

    declaring an object

---
+++ first_app/types.js
@@ -0,0 +1,18 @@
+//declare string (text):
+var vehicle;
+vehicle = "car";
+//declare number:
+var quantity;
+quantity = 1;
+//declare boolean (true, false):
+var isCar = true;
+//declaring an object:
+var user = {
+    name: 'Pepe',
+    age: 40,
+    address: {
+        street: "john flower",
+        zipCode: 841
+    }
+};
+console.log(user);
--- first_app/types.ts
+++ first_app/types.ts
@@ -7,3 +7,21 @@ quantity = 1;
 //declare boolean (true, false):
 let isCar: boolean = true;
 
+//declare an object:
+let user: {
+    name: string,
+    age: number,
+    address: {
+        street: string,
+        zipCode: number
+    }
+} = {
+    name: 'Pepe',
+    age: 40,
+    address: {
+        street: "john flower",
+        zipCode: 841
+    }
+};
+
+console.log(user);
