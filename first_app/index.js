aadfbeb4277691745476ed032411f5569a3523d4
javier-pardinas <javierpb275@gmail.com>
2021-06-25 09:48:36 +0200

    example of declaring varibales type number;

--- first_app/index.js
+++ first_app/index.js
@@ -1,4 +1,9 @@
 var sumTs = function (value1, value2) {
     return value1 + value2;
 };
-console.log(sumTs(1, 2));
+//example of declaring variables type number:
+var value1;
+var value2;
+value1 = 1;
+value2 = 3;
+console.log(sumTs(value1, value2));
--- first_app/index.ts
+++ first_app/index.ts
@@ -2,4 +2,10 @@ const sumTs = (value1: number, value2: number) => {
     return value1 + value2;
 }
 
-console.log(sumTs(1, 2));
+//example of declaring variables type number:
+let value1: number;
+let value2: number;
+value1 = 1;
+value2 = 3;
+
+console.log(sumTs(value1, value2));
