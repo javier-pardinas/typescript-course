34052825353126189e3f4fca49928eef85e1b1d8
javier-pardinas <javierpb275@gmail.com>
2021-06-25 10:18:48 +0200

    declaring arrays and type any

--- first_app/types.js
+++ first_app/types.js
@@ -6,13 +6,21 @@ var quantity;
 quantity = 1;
 //declare boolean (true, false):
 var isCar = true;
-//declaring an object:
+//declare an array (any accepts any type):
+var hobbies;
+hobbies = [123, 'music', 'movies'];
+for (var _i = 0, hobbies_1 = hobbies; _i < hobbies_1.length; _i++) {
+    var hobbie = hobbies_1[_i];
+    console.log(hobbie);
+}
+//declare an object:
 var user = {
     name: 'Pepe',
     age: 40,
     address: {
         street: "john flower",
         zipCode: 841
-    }
+    },
+    courses: ['typescript', 'node.js', 'mongoDB']
 };
 console.log(user);
--- first_app/types.ts
+++ first_app/types.ts
@@ -6,6 +6,13 @@ let quantity: number;
 quantity = 1;
 //declare boolean (true, false):
 let isCar: boolean = true;
+//declare an array (any accepts any type):
+let hobbies: any[];
+hobbies = [123, 'music', 'movies'];
+
+for (const hobbie of hobbies) {
+console.log(hobbie);
+}
 
 //declare an object:
 let user: {
@@ -14,14 +21,16 @@ let user: {
     address: {
         street: string,
         zipCode: number
-    }
+    },
+    courses: string[]
 } = {
     name: 'Pepe',
     age: 40,
     address: {
         street: "john flower",
         zipCode: 841
-    }
+    },
+    courses: ['typescript', 'node.js', 'mongoDB']
 };
 
 console.log(user);
